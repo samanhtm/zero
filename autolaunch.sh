9c56aa5751a858e83a051183fc96cc54ae9116d5
samanhtm <samanhatami997800@gmail.com>
2017-03-15 12:06:05 +0330

    Update autolaunch.sh

--- autolaunch.sh
+++ autolaunch.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./megareborn.sh
-curl "https://api.telegram.org/bot279192153:AAE6V2Stk0lEZPdV1kK5NmQMzPQcDHnLc1s/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot301221764:AAFkVdYC5n4VsQYyrxVcZLyUe-kTSqSJoAc/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
